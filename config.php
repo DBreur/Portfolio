114e7ac1f51952a42f5bafbb3853a1654b72b2e6
Dion Breur <9027852@student.tcrmbo.nl>
2026-05-14 14:37:45 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 // Auteur: Dion Breur
 // Functie: Configuratiebestand
 
-define("SERVERNAME", "localhost");
-define("DATABASE", "portfolio");
-define("USERNAME", "root");
-define("PASSWORD", "");
+define("SERVERNAME", "sql102.infinityfree.com");
+define("DATABASE", "if0_41918408_XXX");
+define("USERNAME", "if0_41918408");
+define("PASSWORD", "XMke6nlsTF5S5");
 
 
 ?>
